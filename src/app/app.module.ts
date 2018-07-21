fec739eb14afa789466a4791d44b1958ba060876
nayakprashanthkumar <nayakprashanthkumar@gmail.com>
2018-07-21 12:00:33 +0530

    Adding

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppsettingsComponent } from './components/appsettings/appsettings.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppheaderComponent,
+    AppfooterComponent,
+    AppmenuComponent,
+    AppsettingsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
